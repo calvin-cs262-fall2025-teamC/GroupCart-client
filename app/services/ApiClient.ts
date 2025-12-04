d7b43b1b3b04c9d69cbd5469503300e0f2c3c5a1
nickneilroberts <154020476+nickneilroberts@users.noreply.github.com>
2025-12-04 01:51:54 -0500

    added a few methods to ApiClient class

--- app/services/ApiClient.ts
+++ app/services/ApiClient.ts
@@ -1,15 +1,14 @@
 import { Favor } from '../models/Favor';
 import { Group } from '../models/Group';
 import { User } from '../models/User';
-import { UserListItem } from '../models/UserListItem';
 
 export class ApiClient {
-  private static BASE_URL: string = `https://groupcart-ggadhpaze4axhxhf.mexicocentral-01.azurewebsites.net/`;
+  private static BASE_URL: string = `https://groupcart-ggadhpaze4axhxhf.mexicocentral-01.azurewebsites.net/api`;
 
   // Send a request to the service, meant to be used by the other methods
   // NOT meant to be used directly within other components. (use methods marked "public" for components)
   private static async request(path: string, method: string = "GET", body?: any) {
-    const response = await fetch(`${this.BASE_URL}${path}`, {
+    const response = await fetch(`${this.BASE_URL}/${path}`, {
       method,
       headers: { "Content-Type": "application/json" },
       body: body ? JSON.stringify(body) : undefined
@@ -24,19 +23,26 @@ export class ApiClient {
   public static async getGroup(id: string): Promise<Group> {
     return this.request(`group/${id}`, "GET");
   }
-  
+
   // Get user info by username
   public static async getUser(username: string): Promise<User> {
     return this.request(`user/${username}`, "GET");
   }
 
-  // Get personal grocery list for a user
-  public static async getUserList(username: string): Promise<UserListItem[]> {
-    return this.request(`list/${username}`, "GET");
-  }
-
   // Get favors fulfilled by a user
   public static async getFavorsByUser(username: string): Promise<Favor[]> {
     return this.request(`favors/by/${username}`, "GET");
+  }
+
+  public static async getFavorsForUser(username: string): Promise<Favor[]> {
+    return this.request(`favors/for/${username}`, "GET");
+  }
+
+  // Add item to List
+  public static async addItemToUserList(
+    username: string,
+    data: { item: string; priority: number }
+  ): Promise<any> {
+    return this.request(`list/${username}`, "POST", data);
   }
 }
