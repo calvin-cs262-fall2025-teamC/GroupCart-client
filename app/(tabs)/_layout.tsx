d73c28034aaaf58e634cb8e9fc8b4710fd48db0c
Aisha Abdullahi <ana9@calvin.edu>
2025-12-04 15:33:22 -0500

    stashed work

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -87,7 +87,6 @@ export default function TabLayout() {
         <Tabs.Screen
           name="index"
           options={{
-            headerTitle:"Shopping List",
             title: "Shopping List",
             tabBarIcon: ({ color, focused }) => (
               <AnimatedTabIcon source={listIcon} color={color} focused={focused} />
